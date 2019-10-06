45f52a70bab8714145a5fbdb3b5eac1a4c80e974
Zarkovic <ivanzarkovic@yahoo.com>
2019-10-07 00:00:37 +0200

    cleaning code

--- app/src/main/java/com/example/shoppingcalculator/ProductsListActivity.java
+++ app/src/main/java/com/example/shoppingcalculator/ProductsListActivity.java
@@ -31,15 +31,13 @@ public class ProductsListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_products_list);
         recyclerView = findViewById(R.id.recycler_view);
         sum = findViewById(R.id.sum);
-
-        //final LinearLayout root = findViewById(R.id.root2);
         db = new DatabaseHelper(this);
         delete = findViewById(R.id.delete);
         bSum = findViewById(R.id.calc);
         bSum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sum.setText(String.valueOf(sum(pricesList)));
+                sum.setText(String.valueOf(sum(pricesList))+" RSD");
             }
         });
         delete.setOnClickListener(new View.OnClickListener() {
@@ -48,52 +46,21 @@ public class ProductsListActivity extends AppCompatActivity {
                 db.deleteAll();
             }
         });
-
-        //tv = findViewById(R.id.tv);
         Cursor result = db.getAllProducts();
         if(result.getCount() == 0){
             return;
         }
-        //StringBuffer strBuff = new StringBuffer();
         while(result.moveToNext()){
             productsList.add(result.getString(1));
             pricesList.add(String.valueOf(0));
-            Log.v("himan", result.getString(1));
-            //strBuff.append("ID: " + result.getString(0) + "\n");
-            //strBuff.append("Name: " + result.getString(1) + "\n");
+            //Log.v("himan", result.getString(1));
         }
-        //pricesList  = new ArrayList<>(productsList.size());
         adapter = new ListAdapter(productsList, pricesList,this);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(adapter);
 
-
-
-//                        for (int i = 0; i < productsList.size(); i++) {
-//                            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//                            final View rowView = inflater.inflate(R.layout.list, null);
-//                            root.addView(rowView, root.getChildCount() - 1);
-//                    View view = root.getChildAt(i);
-//                    TextView et_value = view.findViewById(R.id.list_item);
-//                    et_value.setText(productsList.get(i));
-//                    int value = 0;
-//                    if(et_value!=null){
-//                        try {
-//                            value = Integer.parseInt(et_value.getText().toString().trim());
-//                        }catch (NumberFormatException e){
-//                            Toast.makeText(this, "Unesite broj", Toast.LENGTH_SHORT).show();
-//                        }
-//
-//                    //iSum[0] += value;
-//                    //list.add(value);
-//                    Log.v("Values", " "+value);
-//                    }
-//                }
-                //sum.setText(String.valueOf(sum(list)));
-
-        //tv.setText(strBuff);
     }
 
     private int sum(List<String> list){
